68fb7d1b24963f8716967a89f72282da4411d189
Marc raphael <marcraphael979@gmail.com>
2023-02-03 22:19:20 +0100

    Add starter files

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.scss';
+
+const Header = () => (
+  <div className="Header">
+    <h2>File setup</h2>
+  </div>
+);
+
+export default Header;
