7cf3f718fc8d6c51dd33b68efa0138079a47acce
Marc raphael <marcraphael979@gmail.com>
2023-02-06 14:53:10 +0100

    components/header/Header: Add NavLink

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Navigation from './navigation';
 
 import './header.scss';
+import RecipeFinder from '../assets/RecipeFinder.png';
 
 const Header = () => (
   <div className="Header">
-    <h2>File setup</h2>
+    <NavLink className="logo" to="/"><img src={RecipeFinder} alt="App logo" /></NavLink>
+    <div className="nav-form">
+      <Navigation />
+      <form>
+        <input type="search" placeholder="search for an engredient eg: egg,beef" />
+        <button type="submit">search</button>
+      </form>
+    </div>
   </div>
 );
 
