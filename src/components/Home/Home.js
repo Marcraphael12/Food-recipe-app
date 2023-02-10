4ed27707e5bc6bcbb21d67bb2430c5a9cb834728
Marc raphael <marcraphael979@gmail.com>
2023-02-10 12:05:32 +0100

    components/Home/Home: render category list

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,42 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { NavLink } from 'react-router-dom';
+
+import './home.scss';
+
+import fetchCategories, { joinCategory } from '../../redux/API/features/async_thunk';
+
+export const WorkDone = (dataFetched) => {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.food.food);
+  const foods = Object.values(data);
+  console.log(foods);
+
+  useEffect(() => {
+    dispatch(dataFetched());
+  }, []);
+
+  return foods;
+};
 
 const Home = () => {
   const dispatch = useDispatch();
-
-  useEffect(() => {
-
-  }, [dispatch]);
-  const text = 'Home';
+  const handleJoin = (e) => {
+    dispatch(joinCategory(e.target.id));
+  };
+  const foods = WorkDone(fetchCategories);
   return (
-    <div>
-      <h1>{text}</h1>
-    </div>
+    <ul className="categories">
+      {foods.length <= 1 ? 'Loading...'
+        : foods.map((food) => (
+          <li key={food.category_id} className="category">
+            <img src={food.category_image} alt={food.category_name} />
+            <h2>{food.category_name}</h2>
+            <NavLink to="seemore"><button id={food.category_id} type="button" onClick={handleJoin}>See More</button></NavLink>
+          </li>
+        )) }
+    </ul>
   );
 };
 
