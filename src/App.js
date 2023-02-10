8eac22033dc4c2d284667e2fd205d1eca077f7cd
Marc raphael <marcraphael979@gmail.com>
2023-02-10 12:11:59 +0100

    App: Add route for mockup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/header/Header';
 
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Mockup from './components/Mockup/mockup';
 
 const App = () => (
   <>
@@ -14,6 +15,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="seemore" element={<Mockup />} />
     </Routes>
   </>
 );
